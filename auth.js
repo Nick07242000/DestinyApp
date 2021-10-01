13f89d860c91309e39499cfec06ce948a8448fc2
Nick07242000 <nicohidalgo2000@gmail.com>
2021-10-01 16:51:44 -0300

    Initial Commit

---
+++ auth.js
@@ -0,0 +1,19 @@
+const APIROOTPATH = "https://www.bungie.net/Platform";
+const APIKEY = "590a11a58d0e4f139defe8c5531661ad";
+const AUTHURL = "https://www.bungie.net/en/oauth/authorize";
+const AUTHTOKEN = "https://www.bungie.net/platform/app/oauth/token/";
+const AUTHID = 23872;
+
+function auth() {
+    let req = new Request(`${AUTHURL}?client_id=12345&response_type=code&state=6i0mkLx79Hp91nzWVeHrzHG4`);
+    req.headers.append('X-API-Key',APIKEY);
+    fetch(req).then(response => {
+        console.log(response);
+    })
+}
+
+window.addEventListener("load", function() {
+    document.getElementById("test").addEventListener("click", function() {
+        auth();
+    })
+});
