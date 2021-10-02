fc62be851b76bab395afdc69d4a5ed3d4b970d2b
Nick07242000 <nicohidalgo2000@gmail.com>
2021-10-01 22:58:58 -0300

    Further testing

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 const APIROOTPATH = "https://www.bungie.net/Platform";
-const APIKEY = "590a11a58d0e4f139defe8c5531661ad";
 const AUTHURL = "https://www.bungie.net/en/oauth/authorize";
 const AUTHTOKEN = "https://www.bungie.net/platform/app/oauth/token/";
+const APIKEY = "590a11a58d0e4f139defe8c5531661ad";
 const AUTHID = 23872;
 
 function getParameter(parameterName) {
@@ -10,22 +10,12 @@ function getParameter(parameterName) {
 }
 
 function getToken() {
-    /*
-    console.log(getParameter("code"));
-    let req = new Request(AUTHTOKEN);
-    //separar en dos, pasar url,objeto
-    req.method = POST;
-    req.headers.append('X-API-Key', APIKEY);
-    req.headers.append('Content-Type', 'application/x-www-form-urlencoded')
-    req.body = `client_id=${AUTHID}&grant_type=authorization_code&code=${getParameter("code")}`;
-    */
     fetch(AUTHTOKEN,{
         method:'POST',
         body: `client_id=${AUTHID}&grant_type=authorization_code&code=${getParameter("code")}`,
         headers: {
           'content-type': 'application/x-www-form-urlencoded'
-        },
-        origin: 'https://nick07242000.github.io/DestinyApp/public/'
+        }
     }).then(response => {
         console.log(response);
         response.json().then(data => {
@@ -33,5 +23,3 @@ function getToken() {
         })
     })
 }
-
-document.getElementById("test").addEventListener("click", getToken());
